603c5f7c14be5e7abf4d6f2f3c3afe72afb9fa1b
Nick <kolyan7328@mail.ru>
2023-12-25 21:37:06 +0300

    hw4

--- src/homework4/ex1/Runner.java
+++ src/homework4/ex1/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
     public static void main(String[] args) {
         Car car = new Car(5, 2, 1.8, false, true);
         Car car1 = new Car(5, 2, 1.8, false, true);
-        Car car2= new Car(5, 2, 1.8, false, true);
+        Car car2 = new Car(5, 2, 1.8, false, true);
         Car car3 = new Car(5, 2, 1.8, false, true);
 
         Bus bus = new Bus(12, 3, 2.3, false, 30);
@@ -15,7 +15,6 @@ public class Runner {
 
         CarWash carWash = new CarWash();
         Automobile[] automobiles = {car, car1, car2, car3, bus, bus1, bus2, bus3, bus4};
-        int moneyForWash = carWash.wash(automobiles);
-        System.out.println(moneyForWash);
+        System.out.println(carWash.wash(automobiles));
     }
 }
